2d78f1cad8c74583795b94668ec6f8351a0274b1
Frankline Kiptoo <franklinekiptoo86@gmail.com>
2022-09-21 15:32:16 -0700

    commit

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# Author - Tolulope Fakunle
+# Author - Frankline Kiptoo
 
 for digit1 in range(0, 10):
     for digit2 in range(digit1 + 1, 10):
@@ -7,4 +7,3 @@ for digit1 in range(0, 10):
             print("{}{}".format(digit1, digit2))
         else:
             print("{}{}".format(digit1, digit2), end=", ")
-
